60131c74ff5eb8c07ad662cd8b3fbe160c5226cb
liuzhiyang <zhiyang1.liu@ximalaya.com>
2022-02-05 17:30:40 +0800

    feat: 重新加装lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,165 +1,134 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "plugins": [
-    "@typescript-eslint",
-    "react",
-    "react-hooks"
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-control-statements/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:jsx-control-statements/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
-  ],
-  "env": {
-    "browser": true,
-    "es6": true,
-    "node": true,
-    "jsx-control-statements/jsx-control-statements": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    'jsx-control-statements/jsx-control-statements': true,
   },
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "JSX": true,
-    "parrser": "babel-eslint"
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    JSX: true,
+    parrser: 'babel-eslint',
   },
-  "rules": {
-    // "no-undef": "error",
-    "no-constant-condition": "error",
-    "no-unreachable": "error",
-    "indent": [
-      "error",
+  rules: {
+    'no-constant-condition': 'error',
+    'no-unreachable': 'error',
+    indent: [
+      'error',
       2,
       {
         SwitchCase: 1,
         ignoredNodes: ['ConditionalExpression'],
       },
     ],
-    // "no-duplicate-imports": "error",
-    "@typescript-eslint/no-duplicate-imports": "error",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "jsx-control-statements/jsx-jcs-no-undef": [
-      "off"
-    ],
-    "no-console": [
+    '@typescript-eslint/no-duplicate-imports': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'jsx-control-statements/jsx-jcs-no-undef': ['off'],
+    'no-console': [
       1,
       {
-        "allow": [
-          "warn",
-          "error",
-          "log"
-        ]
-      }
+        allow: ['warn', 'error', 'log'],
+      },
     ],
-    "no-debugger": 1,
-    "import/extensions": 0,
-    "import/no-unresolved": 0,
-    "no-mixed-spaces-and-tabs": 0,
-    "import/no-named-as-default": 0,
-    "import/first": 0,
-    "no-var": "error",
-    "no-unused-expressions": 0,
-    "no-trailing-spaces": 0,
-    "eol-last": 0,
-    "no-restricted-syntax": 0,
-    "max-len": [
-      0,
-      80,
-      4
-    ],
-    "no-plusplus": 0,
-    "no-tabs": "off",
-    "eqeqeq": 0,
-    "import/no-named-as-default-member": 0,
-    "prefer-destructuring": [
-      "error",
+    'no-debugger': 1,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'no-mixed-spaces-and-tabs': 0,
+    'import/no-named-as-default': 0,
+    'import/first': 0,
+    'no-var': 'error',
+    'no-unused-expressions': 0,
+    'no-trailing-spaces': 0,
+    'eol-last': 0,
+    'no-restricted-syntax': 0,
+    'max-len': [0, 80, 4],
+    'no-plusplus': 0,
+    'no-tabs': 'off',
+    eqeqeq: 0,
+    'import/no-named-as-default-member': 0,
+    'prefer-destructuring': [
+      'error',
       {
-        "object": false,
-        "array": false
-      }
+        object: false,
+        array: false,
+      },
     ],
-    "react/prop-types": 0,
-    "react/no-deprecated": 0,
-    "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/no-non-null-assertion": 0,
-    "@typescript-eslint/ban-types": 0,
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/no-empty-interface": 0,
-    "@typescript-eslint/no-this-alias": [
-      0,
-      "self"
-    ],
-    "jsx-control-statements/jsx-use-if-tag": 0,
-    "no-underscore-dangle": 0,
-    "no-restricted-properties": [
+    'react/prop-types': 0,
+    'react/no-deprecated': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/ban-types': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-this-alias': [0, 'self'],
+    'jsx-control-statements/jsx-use-if-tag': 0,
+    'no-underscore-dangle': 0,
+    'no-restricted-properties': [
       2,
       {
-        "object": "require",
-        "message": "Please call require() directly."
-      }
+        object: 'require',
+        message: 'Please call require() directly.',
+      },
     ],
-    "no-alert": 0,
-    "global-require": 0,
-    "no-lone-blocks": 0,
-    // "no-unused-vars": [
-    //   "error",
-    //   {
-    //     "vars": "all",
-    //     "args": "none"
-    //   }
-    // ],
-    "no-extend-native": 0,
-    "import/no-extraneous-dependencies": 0,
-    "arrow-body-style": 0,
-    "func-names": 0,
-    "no-mixed-operators": 0,
-    "prefer-template": 0,
-    "no-use-before-define": 1,
-    "guard-for-in": 1,
-    "camelcase": 0,
-    "no-bitwise": 0,
-    "no-useless-escape": 0,
-    "space-in-parens": 0,
-    "consistent-this": [
-      0,
-      "self"
-    ]
+    'no-alert': 0,
+    'global-require': 0,
+    'no-lone-blocks': 0,
+    'no-extend-native': 0,
+    'import/no-extraneous-dependencies': 0,
+    'arrow-body-style': 0,
+    'func-names': 0,
+    'no-mixed-operators': 0,
+    'prefer-template': 0,
+    'no-use-before-define': 1,
+    'guard-for-in': 1,
+    camelcase: 0,
+    'no-bitwise': 0,
+    'no-useless-escape': 0,
+    'space-in-parens': 0,
+    'consistent-this': [0, 'self'],
   },
-  "overrides": [
+  overrides: [
     {
-      "files": [
-        "**/*.md"
-      ],
-      "parser": "markdown-eslint-parser"
+      files: ['**/*.md'],
+      parser: 'markdown-eslint-parser',
     },
     {
-      "files": [
-        "__tests__/**/*.test.ts",
-        "__tests__/**/*.test.js"
-      ],
-      "plugins": [
-        "jest"
-      ],
-      "extends": [
-        "plugin:jest/recommended"
-      ],
-      "parserOptions": {
-        "jest/globals": true
-      }
-    }, {
-      "files": [
-        '**/*.d.ts'
-      ],
+      files: ['__tests__/**/*.test.ts', '__tests__/**/*.test.js'],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'],
+      parserOptions: {
+        'jest/globals': true,
+      },
+    },
+    {
+      files: ['**/*.d.ts'],
       rules: {
         '@typescript-eslint/no-unused-vars': 0,
-        'no-unused-vars': 0
-      }
-    }
-  ]
-}
+        'no-unused-vars': 0,
+      },
+    },
+    {
+      files: ['__tests__/**/*.test.ts', '__tests__/**/*.test.js'],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'],
+      parserOptions: {
+        'jest/globals': true,
+      },
+    },
+  ],
+};
--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,23 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 // 需要安装ts-node才支持
-import type { InitialOptionsTsJest } from 'ts-jest'
+import type { InitialOptionsTsJest } from 'ts-jest';
 // import { pathsToModuleNameMapper } from 'ts-jest'
-import { defaults as tsjPreset } from 'ts-jest/presets'
+import { defaults as tsjPreset } from 'ts-jest/presets';
 // import tsconfig from './tsconfig.json'; // 这里必须是这个标准的json
 // const { compilerOptions } = tsconfig
 const config: InitialOptionsTsJest = {
+  // automock: true,
   // [...]
-  roots: [
-    "<rootDir>/__tests__"
-  ],
+  roots: ['<rootDir>/__tests__'],
   preset: 'ts-jest/presets/default-esm', // or other ESM presets
   extensionsToTreatAsEsm: ['.ts'],
-  // testEnvironment: "jsdom",
   transform: {
     ...tsjPreset.transform,
+    // "^.+\\.(ts|tsx)$": "typescript-babel-jest"
     // [...]
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // "testRegex": "/__tests__/.*\\.test\\.(ts|tsx)$",
   globals: {
     'ts-jest': {
       useESM: true,
@@ -26,7 +27,8 @@ const config: InitialOptionsTsJest = {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
     // ...pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */) // 跟tsconfig的paths一致
-  }
-}
+  },
+};
+export default config;
 
-export default config
+// __mocks__用来mock node_modules里的模块的
