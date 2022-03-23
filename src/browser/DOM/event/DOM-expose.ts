3840a4188458e7a0278627e5e4eb4659256f4f6b
liuzhiyang <zhiyang1.liu@ximalaya.com>
2022-03-23 15:27:58 +0800

    feat: 增加曝光次数

--- src/browser/DOM/event/DOM-expose.ts
+++ src/browser/DOM/event/DOM-expose.ts
@@ -1,8 +1,19 @@
 import supportPassive from './passive-supported';
-const expose = (dom: Element, cb?: Function, nested = false) => {
+interface Options {
+  nested?: boolean; // 嵌套的滚动监听
+  once?: boolean; // 一次
+}
+const expose = (dom: Element, cb?: Function, options: Options) => {
+  const _options: Options = {
+    nested: false,
+    once: true,
+  };
   const handle = () => {
     const position = dom.getBoundingClientRect();
     if (position.y > 0 && position.y < window.innerHeight) {
+      if (_options.once) {
+        window.removeEventListener('scroll', handle, supportPassive() ? { capture: true } : true);
+      }
       cb && cb();
     }
   };
